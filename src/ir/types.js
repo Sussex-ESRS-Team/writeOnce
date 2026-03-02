562633330dd97ad3079378b0bf8007b4809eb143
tb1819x <109041922+tb1819x@users.noreply.github.com>
2026-03-02 11:13:47 +0000

    MVP of the parser complete, needs reviewing before moving forward

---
+++ src/ir/types.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * WriteOnce Intermediate Representation (IR) Types
+ *
+ * These are application-level TypeScript types intended to be used directly
+ * by parsers, transformers, renderers, and editors.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
